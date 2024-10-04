1f529656214b40f8aa5b21fb74253d527e68e080
jhonrymat <jhonrymat@gmail.com>
2024-10-04 16:08:51 -0500

    quitando boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $cachePath = base_path('bootstrap/cache');
-
-        mkdir($cachePath, 0755, true);
+        //
     }
 }
